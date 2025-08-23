38a9e3fa4bd8e03fde53990a07c1922057cfbb98
Dunghthe04 <tuandungyz09@gmail.com>
2025-08-23 19:01:44 +0700

    Detail product from client

--- controller/client/products.controller.js
+++ controller/client/products.controller.js
@@ -1,9 +1,10 @@
 const Product=require('../../models/product.model')
+//index
 module.exports.index=async(req,res)=>{
     const products=await Product.find({
         status:"active",
         deleted:false
-    });
+    }).sort({position: "desc"});
    const newProducts=products.map(item=>{
      item.newPrice=(item.price - (item.price*(item.discountPercentage/100))).toFixed(0);
      return item;
@@ -12,4 +13,26 @@ module.exports.index=async(req,res)=>{
         pageTitle: "Trang danhs sach san pham",
         products: newProducts 
     })
+}
+
+//detail
+module.exports.detail=async(req,res)=>{
+    try {
+        const find={
+            status:"active",
+            slug: req.params.slug,
+            deleted: false
+        }
+        const product=await Product.findOne(find);
+        if(product){
+          res.render('client/pages/products/detail.pug',{
+            pageTitle: "Chi tiết sản phẩm",
+            product: product
+          })
+        }else{
+          res.redirect("/products")
+        }
+    } catch (error) {
+        res.redirect("/products")
+    }
 }
