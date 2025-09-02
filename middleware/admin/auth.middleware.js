165fbce0cacad730416819f9e66e2945feafd9e8
Dunghthe04 <tuandungyz09@gmail.com>
2025-09-02 13:27:59 +0700

    Authorization(Ủy quyền) xem được truy cập nhưng mục nào

--- middleware/admin/auth.middleware.js
+++ middleware/admin/auth.middleware.js
@@ -1,5 +1,6 @@
 // trang check token
 const Account=require("../../models/accounts.model")
+const Role=require("../../models/roles.model")
 const config=require("../../config/system")
 module.exports.requireAuth=async(req,res,next)=>{
     //nếu chưa có token -> sang trang login
@@ -7,11 +8,17 @@ module.exports.requireAuth=async(req,res,next)=>{
         res.redirect(`${config.PrefixAdmin}/auth/login`)
     }else{
         //nếu có token r , ktra xem token đó có hợp lệ không
-        const account=await Account.find({token: res.cookie.token})
+        const account=await Account.findOne({token: req.cookies.token}).select("-password")
         //nếu ko tman
         if(!account){
           res.redirect(`${config.PrefixAdmin}/auth/login`)
         }else{
+            //nếu thỏa mãn -> trả về thông tin user và các quyền của tài khoản đó để check authen
+            const role=await Role.findOne({_id: account.role_id})
+            console.log(role);
+            
+            res.locals.account=account;
+            res.locals.roleAccount=role;
             next();
         }
     }
