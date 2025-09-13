b3c492376b3bec31c7b94d411c6c11f57f3c8fcb
Dunghthe04 <tuandungyz09@gmail.com>
2025-09-13 12:50:07 +0700

    Hiển thị thông tin cart

--- controller/client/cart.controller.js
+++ controller/client/cart.controller.js
@@ -1,6 +1,37 @@
 
 const Cart=require('../../models/carts.model')
+const Product=require('../../models/product.model')
 const newPriceHelper=require('../../helpers/newPriceProduct')
+
+//index
+module.exports.index=async(req,res)=>{
+    //lây cartId
+    const cartId=req.cookies.cartId;
+    //từ id -> lấy cart
+    const cart=await Cart.findOne({_id: cartId})
+    //duyệt qua từng sản phẩm trong mảng products của cart để lấy thông tin
+    if(cart.products.length>0){
+        for(const item of cart.products){ 
+        //lấy ra thông tin của từng sản phẩm
+        const productInfo=await Product.findOne({_id: item.product_id}).select("title thumbnail slug price discountPercentage")
+        //tạo giá mới cho sp
+        productInfo.newPrice=newPriceHelper.newPrice(productInfo)
+        //lưu thông tin đó vào object
+        item.productInfo=productInfo
+        //tính tổng tiền của 1 object đó
+        item.totalPrice=productInfo.newPrice*item.quantity
+        console.log(item.productInfo);
+      }
+    }
+
+    //tổng tiền của cart
+    cart.totalPriceCart=cart.products.reduce((sum,item)=> sum+item.totalPrice,0)
+    res.render("client/pages/cart/index.pug",{
+        pageTitle: "Giỏ hàng",
+        cart:cart
+    })
+}
+//add
 module.exports.addPost=async(req,res)=>{
     const cartId=req.cookies.cartId;
     const quantity=parseInt(req.body.quantity);
--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const router=express.Router()
 const cartController=require('../../controller/client/cart.controller')
+
+router.get('/',cartController.index)
 router.post('/add/:productId',cartController.addPost)
 
 module.exports=router
