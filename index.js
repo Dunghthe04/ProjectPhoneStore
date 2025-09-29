a8a1963c3662ae00a19a2f19815c1ca4683514ec
Dunghthe04 <tuandungyz09@gmail.com>
2025-09-29 16:17:35 +0700

    Cài đặt socket cho project

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express')
 //env
 require('dotenv').config()
+
 //config
 const systemconfig = require('./config/system.js')
 //method_override(dùng để ghi đè pthuc gửi form lên)
 const methodOverride = require('method-override')
+
 //flash
 const flash = require('express-flash')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
+
 //moment
 const moment = require('moment');
+
+//socket.io
+const http = require('http');
+const { Server } = require("socket.io");
+
 //tinymce
 const path = require('path');
+
 
 //body-parser(dùng để lấy dữ liệu trong req.body)
 const bodyParser = require('body-parser')
@@ -24,18 +33,28 @@ database.connect();
 
 const app = express()
 const port = process.env.PORT
+
+//socket.io
+const server = http.createServer(app);
+const io = new Server(server);
+io.on('connection', (socket) => {
+  console.log('a user connected',socket.id);
+});
 //route
 const clientRoute = require('./routes/client/index.route.js')
 const adminRoute = require('./routes/admin/index.route.js')
+
 //body-parser
 app.use(bodyParser.urlencoded())
 // app.use(bodyParser.urlencoded({extended: false}))
 //method_override
 app.use(methodOverride('_method'))
+
 //pug 
 // app.set('views', './views')
 app.set('views', `${__dirname}/views`)// dùng để online or offline đều đc
 app.set('view engine', 'pug')
+
 //flash
 app.use(cookieParser('dsdsdsd'));
 app.use(session({
@@ -44,6 +63,7 @@ app.use(session({
      }
 }));
 app.use(flash());
+
 //static file
 // app.use(express.static("public"));
 app.use(express.static(`${__dirname}/public`))
@@ -64,6 +84,6 @@ app.get(/.*/,(req,res)=>{
      });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
      console.log(`Example app listening on port ${port}`)
 })
